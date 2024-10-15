53c2b0b4e9ab16c8b479c11b22015ad1791b3553
Hrushikesh Nimbalkar <hrushikesh.nimbalkar@digichorus.com>
2024-10-15 17:58:16 +0530

    security concerns resolved

--- SellMyArt/Controller/LoginVC.swift
+++ SellMyArt/Controller/LoginVC.swift
@@ -6,24 +6,61 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginVC: UIViewController {
+    
+    @IBOutlet weak var emailTxt: UITextField!
+    
+    
+    @IBOutlet weak var passwordTxt: UITextField!
+    
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+       
+    }
+    @IBAction func forPassClcked(_ sender: Any) {
+        
+        
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func lginClcked(_ sender: Any) {
+        signInUsingFirebase()
+        
     }
-    */
+    
+    @IBAction func guestClcked(_ sender: Any) {
+        
+        
+    }
+   
+  func signInUsingFirebase(){
+      guard let email = emailTxt.text, email.isNotEmpty,
+            let password = passwordTxt.text, password.isNotEmpty else{
+                return
+            }
+      
+      Auth.auth().signIn(withEmail: email, password: password) { [weak self] authResult, error in
+        guard let strongSelf = self else { return }
+          
+          if let error = error{
+              print(error.localizedDescription)
+              return
+          }
+          
+          self?.activityIndicator.startAnimating()
+          
+          print("loginSuccessful")
+          
+          DispatchQueue.main.async {
+              self?.activityIndicator.stopAnimating()
+          }
+      }
+    }
 
 }
