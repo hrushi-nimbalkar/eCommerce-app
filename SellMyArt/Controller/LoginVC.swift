690145a0974c1684c843c310c71a37cedcf15ad4
Hrushikesh Nimbalkar <hrushikesh.nimbalkar@digichorus.com>
2024-10-15 18:35:05 +0530

    login first partinitialized

--- SellMyArt/Controller/LoginVC.swift
+++ SellMyArt/Controller/LoginVC.swift
@@ -6,24 +6,64 @@
 //
 
 import UIKit
+import FirebaseCore
+import FirebaseAuth
 
 class LoginVC: UIViewController {
-
+    @IBOutlet weak var emailtxt: UITextField!
+    
+    
+    
+    @IBOutlet weak var passwordtxt: UITextField!
+    
+    @IBOutlet weak var loadingIndicator: UIActivityIndicatorView!
+    
+    @IBOutlet weak var loginbtn: RoundedCorner!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func loginClcked(_ sender: Any) {
+        signInUsingFirebase()
+        
     }
-    */
+    
+    @IBAction func forgotBtnClcked(_ sender: Any) {
+        
+        
+    }
+    
+    @IBAction func continueAsGuestClcked(_ sender: Any) {
+        
+        
+    }
+    
+    
+    func signInUsingFirebase(){
+        guard let email = emailtxt.text, email.isNotEmpty,
+              let password = passwordtxt.text, password.isNotEmpty else{
+            return
+        }
+        loadingIndicator.startAnimating()
+        
+        Auth.auth().signIn(withEmail: email, password: password) { [weak self] authResult, error in
+          guard let strongSelf = self else { return }
+            if let error = error{
+                print("error", error.localizedDescription)
+                DispatchQueue.main.async {
+                    self?.loadingIndicator.stopAnimating()
 
+                }
+                return
+            }
+            
+            print("loginSuccess")
+            DispatchQueue.main.async {
+                self?.loadingIndicator.stopAnimating()
+
+            }
+        }
+    }
 }
